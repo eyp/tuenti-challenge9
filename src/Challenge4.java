0f441b5709ab840a057a4c51c9774a79a2c3df0a
eyp <eduardo.yanez@gmail.com>
2019-05-01 23:40:26 +0200

    Challenge 4 done!

--- src/Challenge4.java
+++ src/Challenge4.java
@@ -1,39 +1,77 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Challenge4 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfSamples = scanner.nextInt();
-        for (int i = 0; i < numberOfSamples; i++) {
+        long numberOfSamples = scanner.nextInt();
+        for (long i = 0; i < numberOfSamples; i++) {
             int numOfEntries = scanner.nextInt();
-            List<Integer> entries = new ArrayList<>();
-            for (int j = 0; j < numOfEntries; j++) {
+            Map<Integer, BigInteger> frequencies = new HashMap<>();
+            int max = 0;
+            for (long j = 0; j < numOfEntries; j++) {
                 int number = scanner.nextInt();
-                entries.add(number);
-            }
-            entries.sort(Comparator.reverseOrder());
-            System.out.println(entries);
-            int x = 1;
-            for (Integer entry : entries) {
-                int f = Collections.frequency(entries, entry);
-                System.out.printf("%d appears %d times, ", entry, f);
-                if (x == 1 && f < entry) {
-                    x = entry - f;
+                if (number >  max) {
+                    max = number;
+                }
+                BigInteger frequency = frequencies.get(number);
+                if (frequency == null) {
+                    frequencies.put(number, BigInteger.ONE);
+                } else {
+                    frequencies.put(number, frequency.add(BigInteger.ONE));
                 }
             }
-            System.out.printf("X = %d\n", x);
-            List<Integer> fullEntries = new ArrayList<>();
-            for (int j = 0; j < x + 1; j++) {
-                fullEntries.addAll(entries);
+            Map<Integer, BigInteger> originalFrequencies = new HashMap<>(frequencies);
+            multiplyAndCheckIsComplete(new ArrayList<>(originalFrequencies.keySet()), originalFrequencies, frequencies);
+            BigInteger people = BigInteger.ZERO;
+            BigInteger candies = BigInteger.ZERO;
+            for (Map.Entry<Integer, BigInteger> entry : frequencies.entrySet()) {
+                people = people.add(entry.getValue().divide(BigInteger.valueOf(entry.getKey())));
+                candies = candies.add(entry.getValue());
             }
-            fullEntries.sort(Comparator.reverseOrder());
-            System.out.println(fullEntries);
+            System.out.printf("Case #%d: %s\n", (i + 1), greatestCommonDenominatorAsString(candies, people));
         }
         scanner.close();
+    }
+
+    private static boolean isComplete(Map<Integer, BigInteger> frequencies) {
+        for (Map.Entry<Integer, BigInteger> entry : frequencies.entrySet()) {
+            if (!entry.getValue().mod(BigInteger.valueOf(entry.getKey())).equals(BigInteger.ZERO)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void multiply(BigInteger x, Map<Integer, BigInteger> originalFrequencies, Map<Integer, BigInteger> frequencies) {
+        for (Map.Entry<Integer, BigInteger> entry : frequencies.entrySet()) {
+            frequencies.put(entry.getKey(), originalFrequencies.get(entry.getKey()).multiply(x));
+        }
+    }
+
+    private static void multiplyAndCheckIsComplete(List<Integer> sequence, Map<Integer, BigInteger> originalFrequencies, Map<Integer, BigInteger> frequencies) {
+        BigInteger x = BigInteger.ONE;
+            for (Integer integer : sequence) {
+                x = x.multiply(BigInteger.valueOf(integer));
+                multiply(x, originalFrequencies, frequencies);
+                if (isComplete(frequencies)) {
+                    break;
+                }
+        }
+    }
+
+
+    private static BigInteger greatestCommonDenominator(BigInteger a, BigInteger b) {
+        return b.equals(BigInteger.ZERO) ? a : greatestCommonDenominator(b, a.mod(b));
+    }
+
+    private static String greatestCommonDenominatorAsString(BigInteger a, BigInteger b) {
+        BigInteger greatestCommonDenominator = greatestCommonDenominator(a, b);
+        return (a.divide(greatestCommonDenominator)) + "/" + (b.divide(greatestCommonDenominator));
     }
 }
