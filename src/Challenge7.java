cb5adecbe1ad243af517999c323263016f967ea0
eyp <eduardo.yanez@gmail.com>
2019-05-02 21:52:28 +0200

    Challenge 7 WIP

--- src/Challenge7.java
+++ src/Challenge7.java
@@ -8,6 +8,7 @@ public class Challenge7 {
         long numberOfSamples = scanner.nextInt();
         Challenge7 challenge7 = new Challenge7();
         for (long i = 0; i < numberOfSamples; i++) {
+            System.out.printf("Case #%d: \n", (i+1));
         }
         scanner.close();
     }
@@ -18,6 +19,7 @@ public class Challenge7 {
         byte[] textBytes = inputText.getBytes(StandardCharsets.ISO_8859_1);
         for (int i = 0; i < textBytes.length; i++) {
             hash[i % 16] = (byte) (hash[i % 16] + textBytes[i]);
+            System.out.printf("i = %d, i %% 16 = %d, bytes = %d, hash = %d\n", i, (i%16), textBytes[i], hash[i%16]);
         }
         return hash;
     }
