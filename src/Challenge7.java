446b2ea948c9505fbcfce8a6babc4cc76d549505
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2019-05-03 16:35:56 +0200

    Challenge 7 WIP

--- src/Challenge7.java
+++ src/Challenge7.java
@@ -1,16 +1,43 @@
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Challenge7 {
+    private static final char[] CRACK_ALPHABET = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    private static final String PRINT_SECTION_SEPARATOR = "---";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long numberOfSamples = scanner.nextInt();
         Challenge7 challenge7 = new Challenge7();
-        for (long i = 0; i < numberOfSamples; i++) {
-            System.out.printf("Case #%d: \n", (i+1));
+        for (int i = 0; i < numberOfSamples; i++) {
+            String originalMessage = readMessage(scanner);
+            String modifiedMessage = readMessage(scanner);
+            System.out.printf("Case #%d: \n", (i + 1));
+            System.out.printf("Original message: %s\n", originalMessage);
+            System.out.printf("Modified message: %s\n", modifiedMessage);
+            System.out.printf("Original message hash: %s\n",
+                              Arrays.toString(challenge7.notSoComplexHash(originalMessage)));
+            System.out.printf("Modified message hash: %s\n",
+                              Arrays.toString(challenge7.notSoComplexHash(modifiedMessage)));
+            challenge7.crackMessage(originalMessage, modifiedMessage);
         }
         scanner.close();
+    }
+
+    private static String readMessage(Scanner scanner) {
+        int numberOfLines = scanner.nextInt();
+        scanner.nextLine();
+        StringBuilder message = new StringBuilder();
+        for (int i = 0; i < numberOfLines; i++) {
+            message.append(scanner.nextLine());
+        }
+        return message.toString();
     }
 
     public byte[] notSoComplexHash(String inputText) {
@@ -19,8 +46,201 @@ public class Challenge7 {
         byte[] textBytes = inputText.getBytes(StandardCharsets.ISO_8859_1);
         for (int i = 0; i < textBytes.length; i++) {
             hash[i % 16] = (byte) (hash[i % 16] + textBytes[i]);
-            System.out.printf("i = %d, i %% 16 = %d, bytes = %d, hash = %d\n", i, (i%16), textBytes[i], hash[i%16]);
+//            System.out.printf("i = %d, i %% 16 = %d, bytes = %d, hash = %d\n", i, (i%16), textBytes[i], hash[i%16]);
         }
         return hash;
+    }
+
+    public byte notSoComplexHashForIndex(String inputText, int index) {
+        byte hash = 0;
+        byte[] textBytes = inputText.getBytes(StandardCharsets.ISO_8859_1);
+        for (int i = (index % 16); i < textBytes.length; i += 16) {
+            hash = (byte) (hash + textBytes[i]);
+//            System.out.printf("i = %d, i %% 16 = %d, bytes = %d, hash = %d\n", i, (i%16), textBytes[i], hash[i%16]);
+        }
+        return hash;
+    }
+
+    public byte[] notSoComplexHashUptoIndex(String inputText, int index) {
+        if (index > 15) {
+            index = 15;
+        }
+        byte[] hash = new byte[16];
+        Arrays.fill(hash, (byte) 0x00);
+        for (int i = 0; i < index + 1; i++) {
+            hash[i] = notSoComplexHashForIndex(inputText, i);
+        }
+        return hash;
+    }
+
+    public String crackMessage(String originalMessage, String modifiedMessage) {
+        byte[] hashOriginalMessage = notSoComplexHash(originalMessage);
+        System.out.printf("Original message hash: %s\n", Arrays.toString(hashOriginalMessage));
+        System.out.printf("Modified message hash: %s\n", Arrays.toString(notSoComplexHash(modifiedMessage)));
+
+        String crack = "";
+        byte[] hashCrackedMessage = notSoComplexHash(buildCrackedMessage(modifiedMessage, crack));
+        long k = 0;
+        while (!areEquals(hashOriginalMessage, hashCrackedMessage) && k < 150) {
+            crack = nextCrack(crack);
+            System.out.printf("Crack [%d]: %s\n", k, crack);
+            String crackedMessage = buildCrackedMessage(modifiedMessage, crack);
+//            System.out.printf("Cracked message: %s\n", crackedMessage);
+            hashCrackedMessage = notSoComplexHash(crackedMessage);
+            System.out.printf("Cracked message hash: %s\n", Arrays.toString(hashCrackedMessage));
+            k++;
+        }
+        System.out.printf("K = %d\n", k);
+        return crack;
+    }
+
+    public String crackMessage3(String originalMessage, String modifiedMessage) {
+        byte[] hashOriginalMessage = notSoComplexHash(originalMessage);
+        System.out.printf("Original message hash: %s\n", Arrays.toString(hashOriginalMessage));
+        System.out.printf("Original message hash value: %d\n", sumByteArray(hashOriginalMessage));
+        System.out.printf("Original message hash value as int: %d\n", sumByteArrayAsInt(hashOriginalMessage));
+
+        String crack = "";
+        byte[] hashCrackedMessage = notSoComplexHash(buildCrackedMessage(modifiedMessage, crack));
+        int k = 0;
+        int printSectionIndex = modifiedMessage.indexOf(PRINT_SECTION_SEPARATOR);
+        int i = 0;
+        String goodCrack = "03W000000S0e0000Xzzwue08BzQz0Z0DzzzzzzRzzzzzez_zz";
+        while (!areEquals(hashOriginalMessage, hashCrackedMessage) && k < 100) {
+            crack += goodCrack.charAt(i++);
+            System.out.printf("Crack [%d]: %s\n", k, crack);
+            String crackedMessage = buildCrackedMessage(modifiedMessage, crack);
+            System.out.printf("Cracked message: %s\n", crackedMessage);
+            hashCrackedMessage = notSoComplexHash(crackedMessage);
+            System.out.printf("Cracked message hash: %s\n", Arrays.toString(hashCrackedMessage));
+            System.out.printf("Cracked message hash value: %d\n", sumByteArray(hashCrackedMessage));
+            System.out.printf("Cracked message hash value as int: %d\n", sumByteArrayAsInt(hashCrackedMessage));
+            k++;
+        }
+        System.out.printf("K = %d\n", k);
+        return crack;
+    }
+
+    public String crackMessage2(String originalMessage, String modifiedMessage) {
+        byte[] hashOriginalMessage = notSoComplexHash(originalMessage);
+
+        String crack = "0";
+        byte[] hashCrackedMessage = notSoComplexHash(buildCrackedMessage(modifiedMessage, crack));
+        int k = 0;
+        int printSectionIndex = modifiedMessage.indexOf(PRINT_SECTION_SEPARATOR);
+        while (!areEquals(hashOriginalMessage, hashCrackedMessage) && k < 100) {
+            int indexAffected = (printSectionIndex + 2 + crack.length()) % 16;
+//            System.out.printf("Hash index affected: %d\n", indexAffected);
+            if (hashCrackedMessage[indexAffected] != hashOriginalMessage[indexAffected]) {
+                crack = nextCrack(crack);
+                System.out.printf("Crack [%d]: %s\n", k, crack);
+                String crackedMessage = buildCrackedMessage(modifiedMessage, crack);
+                System.out.printf("Cracked message: %s\n", crackedMessage);
+                hashCrackedMessage = notSoComplexHash(crackedMessage);
+                System.out.printf("Cracked message hash: %s\n", Arrays.toString(hashCrackedMessage));
+            }
+            k++;
+        }
+        System.out.printf("K = %d\n", k);
+        return crack;
+    }
+
+    public byte[] bytesToReachOriginal(byte original, byte modified, byte start) {
+        byte total = modified;
+        byte currentChar = start;
+        List<Byte> characters = new ArrayList<>();
+        while (total != original) {
+            if (currentChar > 122) {
+                break;
+            }
+            total = (byte) (total + currentChar);
+            characters.add(currentChar);
+            currentChar++;
+        }
+        byte[] bytes = new byte[characters.size()];
+        for (int i = 0; i < characters.size(); i++) {
+            bytes[i] = characters.get(i);
+        }
+        return bytes;
+    }
+
+    public String nextCrack(String crack) {
+        return new String(incrementCrack(crack.getBytes()));
+    }
+
+    public byte[] incrementCrack(byte[] crack) {
+        if (crack.length == 0) {
+            return new byte[]{'0'};
+        }
+
+        int i = 0;
+        for (byte b : crack) {
+            if (b == 'z') {
+                i++;
+                continue;
+            }
+        }
+        if (i == crack.length) {
+            // Add character to sequence
+            byte[] newCrack = new byte[crack.length + 1];
+            Arrays.fill(newCrack, (byte) '0');
+            return newCrack;
+        }
+
+        for (int j = crack.length - 1; j >= 0; j--) {
+            if (crack[j] < 122) {
+                crack[j] = (byte) (crack[j] + 1);
+                if (j < crack.length - 1) {
+                    crack[j + 1] = (byte) '0';
+                }
+                break;
+            }
+        }
+        return crack;
+    }
+
+    public String nextCrack2(String crack) {
+        StringBuilder crackStringBuilder = new StringBuilder(crack);
+        if (crack.length() == 0) {
+            crackStringBuilder.append((char) 48);
+        } else if (crack.charAt(crack.length() - 1) == 'z') {
+            crackStringBuilder.setCharAt(crack.length() - 1, (char) 48);
+            crackStringBuilder.append((char) 48);
+        } else {
+            char currentChar = crackStringBuilder.charAt(crack.length() - 1);
+            crackStringBuilder.setCharAt(crack.length() - 1, ++currentChar);
+        }
+        return crackStringBuilder.toString();
+    }
+
+    private boolean areEquals(byte[] hash, byte[] anotherHash) {
+        return Arrays.equals(hash, anotherHash);
+    }
+
+    private byte sumByteArray(byte[] hash) {
+        byte total = 0;
+        for (byte b : hash) {
+            total += b;
+        }
+        return total;
+    }
+
+    private int sumByteArrayAsInt(byte[] hash) {
+        int total = 0;
+        for (byte b : hash) {
+            total += (int) b;
+        }
+        return total;
+    }
+
+    private String buildCrackedMessage(String modifiedMessage, String crack) {
+        int printSectionIndex = modifiedMessage.indexOf(PRINT_SECTION_SEPARATOR);
+//        System.out.printf("Hash index affected: %d\n", (printSectionIndex + 2 + crack.length()) % 16);
+        String crackedMessage = modifiedMessage.substring(0, printSectionIndex)
+                + PRINT_SECTION_SEPARATOR
+                + crack
+                + modifiedMessage.substring(printSectionIndex + PRINT_SECTION_SEPARATOR.length());
+//        System.out.printf("Cracked message: %s\n", crackedMessage);
+        return crackedMessage;
     }
 }
--- src/Challenge7Test.java
+++ src/Challenge7Test.java
@@ -10,7 +10,7 @@ public class Challenge7Test {
 
         final String card = "Subject: Boat;From: Charlie;To: Desmond;---03W000000S0e0000Xzzwue08BzQz0Z0DzzzzzzRzzzzzez_zz---Penny's boat :)";
         byte[] expected = new byte[] {122, 103, 95, 92, -123, -89, -78, 14, 44, -8, 99, 56, 86, 75, -50, 1};
-//        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHash(card)));
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHash(card)));
 
         final String originalCard = "Subject: Boat;From: Charlie;To: Desmond;------Not Penny's boat";
         expected = new byte[] {122, 103, 95, 92, -123, -89, -78, 14, 44, -8, 99, 56, 86, 75, -50, 1};
@@ -19,6 +19,92 @@ public class Challenge7Test {
 
         final String modifiedCard = "Subject: Boat;From: Charlie;To: Desmond;------Penny's boat :)";
         expected = new byte[] {116, -75, -120, 30, -118, 89, -101, 86, 26, 76, 33, 3, 30, -41, -48, -9};
-        //      Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHash(modifiedCard)));
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHash(modifiedCard)));
+    }
+
+    @Test
+    public void testCrackMessage() {
+        Challenge7 challenge7 = new Challenge7();
+        final String originalCard = "Subject: Boat;From: Charlie;To: Desmond;------Not Penny's boat";
+        final String modifiedCard = "Subject: Boat;From: Charlie;To: Desmond;------Penny's boat :)";
+        String expected = "03W000000S0e0000Xzzwue08BzQz0Z0DzzzzzzRzzzzzez_zz";
+        Assert.assertEquals(expected, challenge7.crackMessage(originalCard, modifiedCard));
+    }
+
+    @Test
+    public void testCrackMessage_EmptyMEssage() {
+        Challenge7 challenge7 = new Challenge7();
+        String originalCard = "------";
+        String modifiedCard = "------";
+        String expected = "";
+//        Assert.assertEquals(expected, challenge7.crackMessage(originalCard, modifiedCard));
+
+        originalCard = "a------";
+        modifiedCard = "b------";
+        expected = "z";
+        Assert.assertEquals(expected, challenge7.crackMessage(originalCard, modifiedCard));
+    }
+
+    @Test
+    public void testBytesToReachOriginal() {
+        Challenge7 challenge7 = new Challenge7();
+        for (byte i = 48; i < 123; i++) {
+            byte[] bytes = challenge7.bytesToReachOriginal((byte) 97, (byte) 98, i);
+            System.out.println(Arrays.toString(bytes));
+        }
+        byte[] bytes = challenge7.bytesToReachOriginal((byte) 97, (byte) 98, (byte) 122);
+        System.out.println(Arrays.toString(bytes));
+    }
+
+    @Test
+    public void testNextCrack() {
+        Challenge7 challenge7 = new Challenge7();
+        Assert.assertEquals("0", challenge7.nextCrack(""));
+        Assert.assertEquals("00", challenge7.nextCrack("z"));
+        Assert.assertEquals("01", challenge7.nextCrack("00"));
+        Assert.assertEquals("10", challenge7.nextCrack("0z"));
+        Assert.assertEquals("11", challenge7.nextCrack("10"));
+        Assert.assertEquals("12", challenge7.nextCrack("11"));
+        Assert.assertEquals("20", challenge7.nextCrack("1z"));
+        Assert.assertEquals("1", challenge7.nextCrack("0"));
+        Assert.assertEquals("1", challenge7.nextCrack("0"));
+        Assert.assertEquals("012B", challenge7.nextCrack("012A"));
+    }
+
+    @Test
+    public void testNotSoComplexHashForIndex() {
+        Challenge7 challenge7 = new Challenge7();
+        String text = "Subject: Boat;From: Charlie;To: Desmond;------Not Penny's boat";
+        Assert.assertEquals(122, challenge7.notSoComplexHashForIndex(text, 0));
+        Assert.assertEquals(103, challenge7.notSoComplexHashForIndex(text, 1));
+        Assert.assertEquals(95, challenge7.notSoComplexHashForIndex(text, 2));
+        Assert.assertEquals(92, challenge7.notSoComplexHashForIndex(text, 3));
+        Assert.assertEquals(-123, challenge7.notSoComplexHashForIndex(text, 4));
+        Assert.assertEquals(-89, challenge7.notSoComplexHashForIndex(text, 5));
+        Assert.assertEquals(-78, challenge7.notSoComplexHashForIndex(text, 6));
+        Assert.assertEquals(14, challenge7.notSoComplexHashForIndex(text, 7));
+        Assert.assertEquals(44, challenge7.notSoComplexHashForIndex(text, 8));
+        Assert.assertEquals(-8, challenge7.notSoComplexHashForIndex(text, 9));
+        Assert.assertEquals(99, challenge7.notSoComplexHashForIndex(text, 10));
+        Assert.assertEquals(56, challenge7.notSoComplexHashForIndex(text, 11));
+        Assert.assertEquals(86, challenge7.notSoComplexHashForIndex(text, 12));
+        Assert.assertEquals(75, challenge7.notSoComplexHashForIndex(text, 13));
+        Assert.assertEquals(-50, challenge7.notSoComplexHashForIndex(text, 14));
+        Assert.assertEquals(1, challenge7.notSoComplexHashForIndex(text, 15));
+        Assert.assertEquals(122, challenge7.notSoComplexHashForIndex(text, 16));
+    }
+
+    @Test
+    public void testNotSoComplexHashUptoIndex() {
+        Challenge7 challenge7 = new Challenge7();
+        String text = "Subject: Boat;From: Charlie;To: Desmond;------Not Penny's boat";
+        byte[] expected = new byte[] {122, 103, 95, 92, -123, -89, -78, 14, 44, -8, 99, 56, 86, 75, -50, 1};
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHashUptoIndex(text, 15)));
+        expected = new byte[] {122};
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHashUptoIndex(text, 0)));
+        expected = new byte[] {122, 103, 95, 92, -123};
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHashUptoIndex(text, 4)));
+        expected = new byte[] {122, 103, 95, 92, -123, -89, -78, 14, 44, -8, 99, 56, 86, 75, -50, 1};
+        Assert.assertEquals(Arrays.toString(expected), Arrays.toString(challenge7.notSoComplexHashUptoIndex(text, 16)));
     }
 }
